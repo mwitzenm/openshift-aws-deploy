556d0e1826347271612bd3770452f692f826f36e
Nick Curry <chasecurry@gmail.com>
2017-08-04 11:15:20 -0500

    Fixed syntax errors

--- run-install.sh
+++ run-install.sh
@@ -4,14 +4,14 @@
 if [ -z ${AWS_ACCESS_KEY_ID} ]; then
     echo "You must set the environment variable 'AWS_ACCESS_KEY_ID'"
     exit 1
-elif [ -z ${AWS_SECRET_ACCESS_KEY ]; then
+elif [ -z ${AWS_SECRET_ACCESS_KEY} ]; then
     echo "You must set the environment variable 'AWS_SECRET_ACCESS_KEY'"
 fi
 
 # Ensure python dependencies are installed
 if ! python -c "import boto" &> /dev/null; then
     echo "The python package 'boto' must be installed"
-fi1
+fi
 
 # Refresh AWS dynamic inventory
 ./inventory/aws/hosts/ec2.py --refresh-cache
